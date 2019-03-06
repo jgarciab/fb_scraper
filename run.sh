c3b1164cf9bb8e6c96a5c68e9ab1a0298211052b
Guido García <guido.garciabernardo@telefonica.com>
2019-03-06 18:41:20 +0100

    chore: enable scripts

--- run.sh
+++ run.sh
@@ -2,14 +2,14 @@
 
 echo "Filter images that contain a face"
 cd face-detector
-#npm install
-#node index.js
+npm install
+node index.js
 cd ..
 
 echo "Resize images"
 cd picture-resizer
-#sh resize-party.sh
-#sh resize-wing.sh
+sh resize-party.sh
+sh resize-wing.sh
 cd ..
 
 echo "Retrain tensorflow models"
